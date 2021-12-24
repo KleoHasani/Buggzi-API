c51464e5c1d7b9a6058299b1809317b2bdb304a6
Kleo <kleo@hasani>
2021-12-24 13:10:48 -0500

    project init

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
 
 const app: Application = express();
+
+app.get("/", (req: Request, res: Response) => {
+  console.log(req);
+  console.log(res);
+  res.json({
+    hello: "world",
+  });
+});
 
 app.listen(8000, () => console.log("Live on port: 8000"));
